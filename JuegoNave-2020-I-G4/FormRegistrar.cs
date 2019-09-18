6bd4b6000c9daba767baea9ba4686899cdff4e7e
faraday58 <jarodriguezvera@gmail.com>
2019-09-18 14:39:26 -0500

    Valida correo

--- JuegoNave-2020-I-G4/FormRegistrar.cs
+++ JuegoNave-2020-I-G4/FormRegistrar.cs
@@ -10,12 +10,16 @@ namespace JuegoNave_2020_I_G4
         private Persona[] personas;
         private byte indPers;
         private Form formpadre;
+        private ErrorProvider errorProvider;
+        private byte TipoError;
 
         public FormRegistrar(Form formpadre)
         {
             this.formpadre = formpadre;
             InitializeComponent();
             personas = new Persona[3];
+            errorProvider = new ErrorProvider();
+
         }
         #region Encapsulamiento
         public byte IndPers {
@@ -43,21 +47,104 @@ namespace JuegoNave_2020_I_G4
             try
             {
                 Persona persona = new Persona();
+                errorProvider.Clear();
                 persona.Nickname = txtbNick.Text;
+                if (persona.Nickname == "")
+                {
+                    string error = "No se puede dejar el campo vacío";
+                    TipoError = 0;
+                    throw new ApplicationException(error);
+                    
+                }
                 persona.Password = txtbPass.Text;
+                if(txtbPass.Text == "" )
+                {
+                    txtbPass.Text = persona.Password;
+                    TipoError = 1;
+                }
+
                 persona.Correo = txtbMail.Text;
+                if(txtbMail.Text == "")
+                {
+                    string error = "No se puede quedar el campo vacío";
+                    TipoError = 2;
+                    throw new ApplicationException(error);
+                }else if(  !persona.Correo.Contains("@")   )
+                {
+                    string error = "Se debe agregar un símbolo '@'";
+                    TipoError = 2;
+                    throw new ApplicationException(error);
+                }else if (!persona.Correo.Contains("."))
+                {
+                    string error = "Se debe agregar un '.'";
+                    TipoError = 2;
+                    throw new ApplicationException(error);
+                }
+                byte pos1 = 0;
+                byte pos2 = 0;
+                for(int i=0; i < persona.Correo.Length; i++)
+                {
+                   char caracter= persona.Correo[i];
+                    if (caracter == 64)
+                    {
+                        pos1 = (byte)i;
+                    }
+                    if (caracter == 46)
+                    {
+                        pos2 = (byte)i;
+                    }
+                }
+                if (pos1 > pos2)
+                {
+                    string error = "NO es valido el correo";
+                    TipoError = 2;
+                    throw new ApplicationException(error);
+                }
+
+
                 persona.Pregunta = cmbPregunta.Items[cmbPregunta.SelectedIndex].ToString();
                 persona.Respuesta = txtbAns.Text;
+                if (txtbAns.Text == "")
+                {
+                    string error = "No se puede quedar el campo vacío";
+                    TipoError = 3;
+                    throw new ApplicationException(error);
+                }
                 personas[IndPers++] = persona;
 
                 this.Hide();
                 formpadre.Show();
             }
-            catch (Exception error)
+            catch (ApplicationException error)
+            {
+                switch (TipoError)
+                {
+                    case 0:
+                        errorProvider.SetError(txtbNick, error.Message);
+                        break;
+                    case 1:
+                        errorProvider.SetError(txtbPass, error.Message);
+                        break;
+                    case 2:
+                        errorProvider.SetError(txtbMail, error.Message);
+                        break;
+                    case 3:
+                        errorProvider.SetError(txtbAns, error.Message);
+                        break;
+
+                }
+                
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                errorProvider.SetError(cmbPregunta, "Debes seleccionar una pregunta");
+
+            }
+            catch
             {
                 MessageBox.Show("Hubo un error");
             }
-                    
+                   
             
         }
 
